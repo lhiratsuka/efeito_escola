6313c2a8615a5caa44167d1e6d99f5bc46bd555b
Livia Kobayashi <48130530+lhiratsuka@users.noreply.github.com>
2023-05-15 15:10:31 -0300

    Delete analises directory

--- analises/render.R
+++
@@ -1,9 +0,0 @@
-params <- new.env()
-params$serie <-  '5EF'
-params$book <-  "../books/saresp5ef_pais_mode.csv"
-
-rmarkdown::render(
-  'teste.Rmd',
-  output_file = "Saresp Pais 5EF.pdf",
-  envir = params)
-
